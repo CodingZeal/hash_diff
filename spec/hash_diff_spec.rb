5e67ce470ea5cff60526ca2f26abde4264053ede
Adam Cuppy <adam@codingzeal.com>
2015-06-23 12:08:11 -0700

    simplify test assertions and replace test doubles in favor of an integration test style

--- spec/hash_diff_spec.rb
+++ spec/hash_diff_spec.rb
@@ -2,38 +2,41 @@ require "spec_helper"
 
 describe HashDiff do
   describe ".diff" do
-    let(:comparison) { double("comparison") }
-    let(:left) { double("left") }
-    let(:right) { double("right") }
+    subject { described_class.diff left, right }
 
-    it "delegates to Comparison#diff" do
-      HashDiff::Comparison.should_receive(:new).with(left, right).and_return(comparison)
-      comparison.should_receive(:diff)
-      HashDiff.diff(left, right)
-    end
+    let(:left) {
+      { foo: "bar" }
+    }
+    let(:right) {
+      { foo: "bar2" }
+    }
+
+    it { expect(subject).to eq({ foo: ['bar', 'bar2']}) }
   end
 
   describe ".left_diff" do
-    let(:comparison) { double("comparison") }
-    let(:left) { double("left") }
-    let(:right) { double("right") }
-    
-    it "delegates to Comparison#left_diff" do
-      HashDiff::Comparison.should_receive(:new).with(left, right).and_return(comparison)
-      comparison.should_receive(:left_diff)
-      HashDiff.left_diff(left, right)
-    end
+    subject { described_class.left_diff left, right }
+
+    let(:left) {
+      { foo: "bar" }
+    }
+    let(:right) {
+      { foo: "bar2" }
+    }
+
+    it { expect(subject).to eq({ foo: 'bar2' }) }
   end
 
   describe ".right_diff" do
-    let(:comparison) { double("comparison") }
-    let(:left) { double("left") }
-    let(:right) { double("right") }
-    
-    it "delegates to Comparison#right_diff" do
-      HashDiff::Comparison.should_receive(:new).with(left, right).and_return(comparison)
-      comparison.should_receive(:right_diff)
-      HashDiff.right_diff(left, right)
-    end
+    subject { described_class.right_diff left, right }
+
+    let(:left) {
+      { foo: "bar" }
+    }
+    let(:right) {
+      { foo: "bar2" }
+    }
+
+    it { expect(subject).to eq({ foo: 'bar' }) }
   end
 end
