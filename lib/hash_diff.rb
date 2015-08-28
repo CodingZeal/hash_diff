3282b6a26e802a78c7ed1cd3f4d0067740dcb0a4
James Edward Gray II <james@graysoftinc.com>
2015-08-28 09:00:27 -0500

    Switching to a more standard dual interface so the methods can be mixed into other API's, if desired.

--- lib/hash_diff.rb
+++ lib/hash_diff.rb
@@ -2,25 +2,25 @@ require "hash_diff/version"
 require "hash_diff/comparison"
 
 module HashDiff
-  class << self
-    def diff(*args)
-      Comparison.new(*args).diff
-    end
+  def self.patch!
+    Hash.class_eval do
+      def diff(right)
+        HashDiff.left_diff(self, right)
+      end
+    end unless Hash.new.respond_to?(:diff)
+  end
 
-    def left_diff(*args)
-      Comparison.new(*args).left_diff
-    end
+  module_function
 
-    def right_diff(*args)
-      Comparison.new(*args).right_diff
-    end
+  def diff(*args)
+    Comparison.new(*args).diff
+  end
 
-    def patch!
-      Hash.class_eval do
-        def diff(right)
-          HashDiff.left_diff(self, right)
-        end
-      end unless Hash.new.respond_to?(:diff)
-    end
+  def left_diff(*args)
+    Comparison.new(*args).left_diff
+  end
+
+  def right_diff(*args)
+    Comparison.new(*args).right_diff
   end
 end
